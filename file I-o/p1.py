c91cda893ff98a139222fe3fda8a6ed2c9de9747
Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-06-25 17:38:26 +0530

    Add hello.json and update p1.py to read JSON data with error handling

--- file I-o/p1.py
+++ file I-o/p1.py
@@ -1,10 +1,10 @@
 # with statement is used to wrap the execution block within the context manager
-
-file_path = "C:/Users/1000/OneDrive/Documents/python_master_grandplan/file I-o/hello.txt"
+import json
+file_path = "hello.json"
 try: 
     with open(file_path, "r") as file:
-        content = file.read()
-        print(content)
+        content = json.load(file)
+        print(content["greatone"])
 except FileNotFoundError:  # Changed 'expect' to 'except'
     print("hero kya baat hai")  # Indented this line
 except PermissionError:
